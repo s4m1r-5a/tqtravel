1b0aaa7e6d69d89ca431b455461dc93d5f292c4f
RedFlix <r3dflix@gmail.com>
2020-09-16 20:53:30 -0500

    redflix

--- src/keys.js
+++ src/keys.js
@@ -26,12 +26,13 @@ module.exports = {
         categoria: 'i'
     },
     Google: {
-        client_id: "358691758390-ea426ocipho2d13q7aku48q3gmo9ktal.apps.googleusercontent.com",
+        ApiKey: 'AIzaSyA_2TwyNJiGQwKXDVHOM4_I3jg69qvHc-Y',
+        client_id: "507038552414-0d2oul1ks021a2ajvh7p2771qmoel9ln.apps.googleusercontent.com", // 358691758390-ea426ocipho2d13q7aku48q3gmo9ktal.apps.googleusercontent.com
         project_id: "still-toolbox-253319",
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
         token_uri: "https://oauth2.googleapis.com/token",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_secret: "lIFar1pydYRrcArlKxTe3S8h",
+        client_secret: "QRAaQqm5PmIA55bw6N2-hpE2", // lIFar1pydYRrcArlKxTe3S8h
         redirect_uris: [
             "https://tqtravel.herokuapp.com/auth/google/callback",
             "http://localhost:3000/auth/google/callback"
@@ -55,7 +56,7 @@ module.exports = {
         otro: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.MTI4Mg.RO7HFV11U1YNtFNpPnCcOIaQcHU72f7tPn3HoOCMXOg'
     },
     Calendario: {
-        client_secret: 'W8_LvfwX552MdYbzN0wfzgGx',
+        client_secret: 'QRAaQqm5PmIA55bw6N2-hpE2', // W8_LvfwX552MdYbzN0wfzgGx
         client_id: '281961950966-dqv72su3fjcbhi003no1vp1rrattf8t9.apps.googleusercontent.com',
         redirect_uris: 'http://localhost:3000/calendario'
     }
